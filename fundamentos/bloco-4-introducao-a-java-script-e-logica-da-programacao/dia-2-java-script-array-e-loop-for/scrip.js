e2b286d1c6662e96a5f777cf50f03dae79eb7d66
Alissandra <alissandraraysa@gmail.com>
2021-08-26 10:17:48 -0300

    exercicio 4

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-2-java-script-array-e-loop-for/scrip.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-2-java-script-array-e-loop-for/scrip.js
@@ -1,14 +1,20 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 0; i < numbers.length; i++){
-    console.log(numbers[i])
+    console.log(numbers[i]);
 };
 
 let soma =0;
 
 for (let i= 0; i <numbers.length; i++){
-   soma  = (soma+numbers[i])
+   soma  = (soma+numbers[i]);
 }
-console.log (soma)
+console.log (soma);
 
-console.log (soma/numbers.length)
+let mediAritimetica = (soma/numbers.length);
+
+if (mediAritimetica > 20){
+    console.log("valor maior que 20");
+} else {
+   console.log("valor menor ou igual a 20") 
+}
