1ca78421dd9a470f142b603b3630087c0c77e153
Alissandra <alissandraraysa@gmail.com>
2021-08-26 15:32:45 -0300

    adiciona questoes 1, 8 e 9

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-2-java-script-array-e-loop-for/scrip.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-2-java-script-array-e-loop-for/scrip.js
@@ -1,3 +1,4 @@
+//questao 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 0; i < numbers.length; i++){
@@ -6,18 +7,26 @@ for (let i = 0; i < numbers.length; i++){
 
 let soma =0;
 
+//questao 2
+
 for (let i= 0; i <numbers.length; i++){
    soma  = (soma+numbers[i]);
 }
 console.log (soma);
 
+//questao 3
+
 let mediAritimetica = (soma/numbers.length);
+
+//questao 4
 
 if (mediAritimetica > 20){
     console.log("valor maior que 20");
 } else {
    console.log("valor menor ou igual a 20") 
 }
+
+//questao 5
 
 let maior = numbers[0]
 
@@ -27,6 +36,49 @@ for (let i=1; i< numbers.length; i++){
     }
 }
 
-
 console.log (maior)
+
+//questao 6
+
+let qtdImpares = 0;
+
+for (let i=0; i< numbers.length; i++){
+    if( numbers[i]% 2 !== 0){
+        qtdImpares += 1
+    }
+}
+   if (qtdImpares == 0){
+       console.log("nenhum valor ímpar encontrado")
+   }
+
+console.log(qtdImpares)
+
+//questao 7
+
+let menor = numbers[0];
+
+for (let i= 1; i< numbers.length; i++){
+  if (numbers[i] < menor){
+      menor = numbers[i]
+  }
+}
+
+console.log (menor)
+
+//questao 8
+
+let questao8 = [];
+
+for (let i = 0; i <= 25; i ++){
+    questao8.push(i)
+}
+
+console.log (questao8)
+
+//questao 9
+
+for (let i =0; i < questao8.length; i++){
+    console.log(questao8[i]/2)
+}
+
 
