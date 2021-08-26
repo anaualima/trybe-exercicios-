b4d3f0081efa702153d3f9813469b45ca6252a5b
Alissandra <alissandraraysa@gmail.com>
2021-08-25 21:15:10 -0300

    adciona exercicios

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-1-primeiros-passos/exercicio.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-1-primeiros-passos/exercicio.js
@@ -7,14 +7,25 @@ let multiplicacao = a*b;
 let divisao = a/b;
 let modulo = a%b;
 
-const h = 30;
-const i = 40;
+const c = 30;
+const d = 40;
 
-if (h > i){
-    console.log (h)
+if (c > d){
+    console.log (c)
 } else {
-    console.log (i)
+    console.log (d)
 }
 
+const e = 3;
+const f = 4;
+const g = 5;
+
+if (e > f){
+    console.log (e)
+} else if (f > g){
+    console.log(f)
+} else {
+    console.log(g)
+}
 
 
