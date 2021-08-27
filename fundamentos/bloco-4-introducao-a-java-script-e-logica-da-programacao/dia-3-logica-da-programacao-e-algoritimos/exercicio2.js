92ac61978516e0a08e3f6c9f75e9bee114747770
Alissandra <alissandraraysa@gmail.com>
2021-08-27 14:33:29 -0300

    adciona exercicios 1 e 2

---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-3-logica-da-programacao-e-algoritimos/exercicio2.js
@@ -0,0 +1,14 @@
+//criar a figura de uma triangulo a esquerda com asteríscos dado um vamos n, onde n é sempre maior que 1
+
+let n = 5;
+let triangulo = "" ;
+let asterisco = "*";
+
+for (let i=0; i < n; i+=1){
+    triangulo = triangulo + asterisco
+    console.log (triangulo)
+}
+
+// for (let i=0; i < n; i+=1){
+//     console.log(triangulo)
+// }
