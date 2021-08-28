5ec8cf6dacef22ea3329de764b95940769599afe
Alissandra <alissandraraysa@gmail.com>
2021-08-28 13:06:02 -0300

    exemplo de switch case

---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-1-primeiros-passos/exemploswitchcase.js
@@ -0,0 +1,15 @@
+let pessoa = "lista"
+
+switch (pessoa){
+    case "aprovada":
+        console.log("parabéns");
+        break;
+    case "reprovada":
+        console.log("infelizmente");
+        break;
+    case "lista":
+        console.log("espere");
+        break;
+    default:
+        console.log("nãose aplica");
+}
