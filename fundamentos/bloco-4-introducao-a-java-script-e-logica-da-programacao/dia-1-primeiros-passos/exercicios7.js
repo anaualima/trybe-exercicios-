195f5101b006211ef7aa8176b0cd55ddf090a1d1
Alissandra <alissandraraysa@gmail.com>
2021-08-28 14:12:34 -0300

    questao 7

---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-1-primeiros-passos/exercicios7.js
@@ -0,0 +1,17 @@
+//exercicio 7 nota dada em porcentagem que retorne os conceitos de A a F.
+
+let nota = 40;
+
+if (nota >=90){
+    console.log("A")
+} else if (nota >=80){
+    console.log("B")
+} else if (nota >=70){
+    console.log("C")
+} else if (nota >=60){
+    console.log("D")
+} else if (nota >=50){
+    console.log("E")
+} else {
+    console.log("F")
+} 
