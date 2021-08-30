347a6ba77100950f0e047ede14e6d96b3efad4ad
Alissandra <alissandraraysa@gmail.com>
2021-08-30 15:32:11 -0300

    adciona exemplo 3

---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-4-objatos-e-funcoes/exemplo3.js
@@ -0,0 +1,21 @@
+//exemplo 3
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+
+  for (let key in names){
+    console.log("olá, "+ names[key]);
+  }
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (key in car){
+      console.log(car);
+  }
