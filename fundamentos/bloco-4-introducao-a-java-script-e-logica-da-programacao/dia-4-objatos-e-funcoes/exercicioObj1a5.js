632c5c17d80c6cebad411f972f1e90d2d2b69b9d
Alissandra <alissandraraysa@gmail.com>
2021-08-30 18:44:18 -0300

    adiciona exercicios obj de 1 a 5

---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-4-objatos-e-funcoes/exercicioObj1a5.js
@@ -0,0 +1,51 @@
+//exercicio 1 de objeto
+
+
+//exercicio 1
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log ("Bem vinda! "+info.personagem);
+
+// exercicio 2
+
+  info ['recorrente'] = 'sim'
+
+  for (let key in info){
+      console.log(info)
+  }
+//exercicio 3
+
+  for (let key in info){
+      console.log(key)
+  }
+
+//exericio 4
+
+  for (let key in info){
+      console.log(info[key])
+  }
+
+//exercicio 5
+
+let tio = {
+
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell' s Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'sim'
+}
+
+for (let key in info){
+if (key === "recorrente" && tio.recorrente === 'sim' && info.recorrente === 'sim'){
+    console.log ('Ambos são recorrentes')
+}else{
+    console.log(info[key] + tio[key])
+}
+}
+
+
+
