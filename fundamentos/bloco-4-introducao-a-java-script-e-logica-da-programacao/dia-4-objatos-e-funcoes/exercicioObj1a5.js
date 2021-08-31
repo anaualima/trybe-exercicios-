323b3d94fc05eeb466363661f05bf9ba75a2f1d5
Alissandra <alissandraraysa@gmail.com>
2021-08-30 22:25:22 -0300

    adciona exercicios funcoes

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-4-objatos-e-funcoes/exercicioObj1a5.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-4-objatos-e-funcoes/exercicioObj1a5.js
@@ -47,5 +47,3 @@ if (key === "recorrente" && tio.recorrente === 'sim' && info.recorrente === 'sim
 }
 }
 
-
-
---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-4-objatos-e-funcoes/exerciciofuncao1.js
@@ -0,0 +1,22 @@
+//cria uma função para palindramo
+
+let palavra = "arara";
+let resultado = "";
+
+if (palavra.reverse === palavra) {
+resultado = "true"
+} else {
+    resultado = "false"
+}
+
+console.log(resultado)
+
+
+
+    // let array = palavra.split();
+    // console.log (array);
+
+    // let reverso = array.reverse();
+    // console.log (reverso);
+
+
