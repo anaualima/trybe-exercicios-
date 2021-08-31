b7c8e24953dc874cf28f40912f0e9e0d1a34966c
Alissandra <alissandraraysa@gmail.com>
2021-08-31 10:31:38 -0300

    inicia exercicio 5

---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-da-programacao/dia-4-objatos-e-funcoes/exercicioFuncao5.js
@@ -0,0 +1,10 @@
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete
+
+let numeros = [5, 8, 2, 3, 3];
+let primeiro = numeros[0];
+let maisSeRepete = 0;
+
+for (let i = 1; i < numeros.length; i+=1){
+    
+}
+console.log(maisSeRepete)
