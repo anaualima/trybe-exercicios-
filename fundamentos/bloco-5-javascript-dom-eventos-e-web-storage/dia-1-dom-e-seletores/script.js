65047b27a6d169ee8ac637e1ba15457275aa6f8f
Alissandra <alissandraraysa@gmail.com>
2021-09-02 14:54:23 -0300

    exemplo sobre dom, getElementById e innerText

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/script.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Como estrelas na terra, todas as crianças são especiais";
+document.getElementById("paragraph").innerText = "Um filme Bollywoodiano que trata da relação entre uma criança dislexa e seu professor.";
+document.getElementById("subtitle").innerText = "Vale a pena assistir!";
