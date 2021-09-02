5f3a1295774289b816f3882e3fc1f6f8e45dccf0
Alissandra <alissandraraysa@gmail.com>
2021-09-02 15:12:15 -0300

    altera segundo paragrafo

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/script.js
@@ -1,3 +1,4 @@
 document.getElementById("page-title").innerText = "Como estrelas na terra, todas as crianças são especiais";
 document.getElementById("paragraph").innerText = "Um filme Bollywoodiano que trata da relação entre uma criança dislexa e seu professor.";
 document.getElementById("subtitle").innerText = "Vale a pena assistir!";
+document.getElementById("second-paragraph").innerText = "Taare Zameen Par (em hindi: तारे ज़मीन पर; em inglês: Like Stars on Earth) é um filme indiano de 2007, dos gêneros infantil e comédia dramática, dirigido por Aamir Khan, que também atua, no papel do professor Nikumbh.O filme estreou nos cinemas em 21 de dezembro de 2007 e foi lançado em DVD em 2008 na Índia como Taare Zameen Par e em 2010.";
