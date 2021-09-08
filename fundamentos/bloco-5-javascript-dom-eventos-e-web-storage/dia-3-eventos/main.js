915a51658fc925dd8e806308dc90364a37556b11
Alissandra <alissandraraysa@gmail.com>
2021-09-08 19:14:50 -0300

    adciona exercicio 4

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/main.js
@@ -10,12 +10,32 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+function handleChangeTech(event) {
+    let elementoAserUsado = document.querySelector('.tech');
+    techElement.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+  }
+  
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener("input", mudaTexto)
+
+function mudaTexto(event){
+    let elementoAserUsado = document.querySelector('.tech');
+    elementoAserUsado.innerText = event.target.value
+}
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+let meuTopSpotrybe = document.querySelector('h3');
+meuTopSpotrybe.addEventListener("dblclick", duploCick)
+
+function duploCick(event){
+    //let meuTopSpotrybe = document.querySelector('h3');
+    window.location.replace('https://anaualima.github.io/')
+}
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
