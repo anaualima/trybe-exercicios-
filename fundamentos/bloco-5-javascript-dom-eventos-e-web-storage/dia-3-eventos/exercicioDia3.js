cbd7c0449971d1a4ce36ccf39313c1c91eb96407
Alissandra <alissandraraysa@gmail.com>
2021-09-09 10:20:52 -0300

    segunda questao, adiciona botao

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/exercicioDia3.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/exercicioDia3.js
@@ -17,6 +17,11 @@ function createDaysOfTheWeek() {
 
   //O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
 
+  //Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+  //Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+  //Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+
+
   let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   let dezembro = document.getElementById("days");
 
@@ -37,6 +42,18 @@ function createDaysOfTheWeek() {
       }
   }
 
-  //Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
-  //Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
-  //Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+  //Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+  //Adicione a este botão a ID "btn-holiday" .
+  //Adicione este botão como filho/filha da tag <div> com classe "buttons-container" 
+
+  function recebeFeriados (palavra){
+      let guardaBotao = document.querySelector('.buttons-container')
+      let botaoFeriado = document.createElement('button');
+      botaoFeriado.innerText = palavra;
+      botaoFeriado.id = "btn-holiday";
+      guardaBotao.appendChild(botaoFeriado);
+  }
+
+  recebeFeriados("Feriados");
+
+  
