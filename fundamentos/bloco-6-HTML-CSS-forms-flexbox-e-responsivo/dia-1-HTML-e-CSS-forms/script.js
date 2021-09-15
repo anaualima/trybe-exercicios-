3809e9068897f0997a8255b7cf9b4e0285cc5f96
Alissandra <alissandraraysa@gmail.com>
2021-09-15 19:12:16 -0300

    exercicio do dia 6.1

---
+++ fundamentos/bloco-6-HTML-CSS-forms-flexbox-e-responsivo/dia-1-HTML-e-CSS-forms/script.js
@@ -0,0 +1,8 @@
+let gaurdaBotoes = document.getElementById("#container-button");
+let botaoEnviar = document.getElementById("#button-submit")
+
+botaoEnviar.addEventListener("click", enviarFormulario)
+
+function enviarFormulario (){
+
+}
