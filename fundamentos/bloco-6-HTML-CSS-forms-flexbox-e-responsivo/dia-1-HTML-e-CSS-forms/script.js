aa109110bcb25759ccf4710eb4eb59a817c9b6d1
Alissandra <alissandraraysa@gmail.com>
2021-09-17 10:42:33 -0300

    adiciona data em formato de texto no forms

--- fundamentos/bloco-6-HTML-CSS-forms-flexbox-e-responsivo/dia-1-HTML-e-CSS-forms/script.js
+++ fundamentos/bloco-6-HTML-CSS-forms-flexbox-e-responsivo/dia-1-HTML-e-CSS-forms/script.js
@@ -1,8 +1,37 @@
-let gaurdaBotoes = document.getElementById("#container-button");
-let botaoEnviar = document.getElementById("#button-submit")
+//função data 17/09/21
 
-botaoEnviar.addEventListener("click", enviarFormulario)
+let dataEmTexto = document.getElementById("date");
+let guardarData = document.getElementsByTagName('fieldset')[1]
+let botaoEnviar =document.getElementById('submit');
 
-function enviarFormulario (){
+botaoEnviar.addEventListener("click", criarTipoDeData);
+
+function criarTipoDeData(event){
+    event.preventDefault();
+
+    if(dataEmTexto.value.length < 10){
+        alert("a data está muito curta");
+    }
+    let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
+    if(!regex.test(dataEmTexto.value)){
+        alert("data no formato inválido!");
+    }
+
+    let splitted = dataEmTexto.value.split('/');
+    let day = splitted[0];
+    let month = splitted[1];
+    let year = splitted[2];
+
+    if(day<0 || day >31){
+        alert("dia inválido!");
+    }
+
+    if(month<0 || month>12){
+        alert("mês inválido");
+    }
+
+    if(year<0){
+        alert("ano inválido!");
+    }
 
 }
