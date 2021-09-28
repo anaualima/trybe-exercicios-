4d4aa9a8595219e6753269ba09f1c7bc326299f5
Alissandra <alissandraraysa@gmail.com>
2021-09-27 22:16:57 -0300

    ultimo commit do dia

--- fundamentos/bloco-7-introducao-a-javascript-ESe-testes-unitarios/dia-2-es6-objects/exercicio25.js
+++ fundamentos/bloco-7-introducao-a-javascript-ESe-testes-unitarios/dia-2-es6-objects/exercicio25.js
@@ -36,4 +36,16 @@ function chaveArray(objeto,num) {
     return chaves = Object.values(objeto)[num];
 }
 
-console.log(chaveArray(lesson1, 0));
+//console.log(chaveArray(lesson1, 0));
+
+function verify (objeto,chave,valor) {
+    let par = Object.entries(objeto);
+    for (let i in par){
+    if (par[i[0]] == chave && par[i[1]] == valor){
+        return true
+    } else{
+        return false
+    }
+ }
+}
+console.log(verify(lesson3, 'turno', 'noite'));
