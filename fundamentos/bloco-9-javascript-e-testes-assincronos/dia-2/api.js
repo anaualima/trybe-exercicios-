dddef6cdef003cd7369dae89fd3ce18a936b9092
Alissandra <alissandraraysa@gmail.com>
2021-10-19 14:58:03 -0300

    exercicios

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2/api.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2/api.js
@@ -1,12 +1,22 @@
 // criptomoedas
-const API_URL = 'api.coincap.io/v2/assets';
+const API_URL = 'https://api.coincap.io/v2/assets';
+console.log(API_URL);
 
 function getCoins() {
-    fetch(API_URL)
+  return fetch(API_URL)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => data)
+    .catch(error => console.log(error));
 }
 
-getCoins()
+async function nome() {
+  const {data} = await getCoins();
+  const coins = document.getElementById('criptomoedas')
+  data.filter((_, i) => i<10).forEach((coin) => {
+    const li = document.createElement('li');
+    li.innerText = `${coin.name} (${coin.symbol}): ${Number(coin.priceUsd).toFixed(2)}`
+    coins.appendChild(li)
+  });
+}
 
-window.onload = () => getCoins();
+nome();
