667c5606185d2a940f6997f800d844c0f7391851
Alissandra <alissandraraysa@gmail.com>
2021-10-28 05:42:27 -0300

    exercicio 1

---
+++ front-end/bloco-10-introducao-ao-react/dia-1-react/dia-01/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './App.css';
+
+const Task = (value) => {
+  const valor = [<li>Observar</li>, <li>Escrever</li>, <li>Refletir</li>, <li>Criticar</li>];
+  value = valor.map((v) => v);
+  return (
+    <ul>{value}</ul>
+  );
+}
+
+function App() {
+  return (<Task/>);
+}
+
+export default App;
+
+
+// import React from 'react';
+// import './App.css';
+
+// const Task = (value) => {
+//   return (
+//     <li key={value}>{value}</li>
+//   );
+// }
+
+// const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <ul>{ tarefas.map((tarefa) => Task(tarefa)) }</ul>
+//     );
+//   }
+// }
+
+// export default App;
