11ec2fc552a75b15cec0d014837490d63a9dc6df
Alissandra <alissandraraysa@gmail.com>
2021-11-10 08:32:23 -0300

    exemplo 1

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-rounter/dia-2-react-router/exemplo-course-1/src/HowTo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HowTo extends Component {
+    render() {
+        return (
+            <div>
+                <p>HowTo: como será que funciona?</p>
+            </div>
+        )
+    }
+}
---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-rounter/dia-2-react-router/exemplo-course-1/src/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <div>
+                <p>Profile: vai dar certo!</p>
+            </div>
+        )
+    }
+}
