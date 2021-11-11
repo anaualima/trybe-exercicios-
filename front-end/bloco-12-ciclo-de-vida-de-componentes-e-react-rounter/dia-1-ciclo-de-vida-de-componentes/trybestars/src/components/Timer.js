ccf5ebc2c2c9b80c275928999f1f2539c76230e1
Alissandra <alissandraraysa@gmail.com>
2021-11-11 11:11:16 -0300

    restante do conteudo da aula, aprendendo a fazer um timer de 5 segundos

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-rounter/dia-1-ciclo-de-vida-de-componentes/trybestars/src/components/Timer.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-rounter/dia-1-ciclo-de-vida-de-componentes/trybestars/src/components/Timer.js
@@ -1,16 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+const secondsChange = 1000;
+const timeLimit = 5;
 
 export default class Timer extends Component {
     constructor (){
         super()
         this.state = {
-          seconds: 5,
+          seconds: 1,
+          phases: ['INSPIRE...', 'SEGURE...', 'EXPIRE...'],
+          currentPhase: 0,
         }
     }
+
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState((prevState) => ({
+                seconds: prevState.seconds+1,
+            }))
+        }, secondsChange)
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.seconds === timeLimit){
+            this.setState({
+                seconds: 1,
+                currentPhase: prevState.currentPhase === 2? 0 : prevState.currentPhase+1,
+            })
+        }
+    }
+
+    componentWillUnmount() {  // DESMONTA O COMPONENTE;
+        clearInterval(this.intervalId); 
+    }
+
     render() {
-        const { seconds } = this.state
+        const { seconds, phases, currentPhase } = this.state
         return (
             <section>
+                <h1>{phases[currentPhase]}</h1>
                 <h2>{seconds}</h2>
             </section>
         )
