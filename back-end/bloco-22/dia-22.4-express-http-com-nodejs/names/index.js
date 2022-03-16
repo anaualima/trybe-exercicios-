b1302b870200071f892d4f8fc6e46c8b1d1a52dc
Alissandra <alissandraraysa@gmail.com>
2022-03-16 19:12:31 -0300

    adiciona leitura de erro 500

--- back-end/bloco-22/dia-22.4-express-http-com-nodejs/names/index.js
+++ back-end/bloco-22/dia-22.4-express-http-com-nodejs/names/index.js
@@ -3,17 +3,23 @@ const fs = require('fs/promises');
 
 const app = express();
 const PORT = 3000;
-const FILENAME = 'names.txt';
+const FILENAME = 'name.txt';
 
 app.get('/hello', (req, res, next) => {
-    res.status(200).send({message:'hello world'});
+  res.status(200).send({ message: 'hello world' });
 });
 
 app.get('/names', async (req, res, next) => {
+  try {
     const data = await fs.readFile(FILENAME);
     const names = data.toString().split('\n');
 
-    res.status(200).send({names:names})
+    res.status(200).send({ names: names })
+
+  } catch (e) {
+
+    res.status(500).send({ message: 'erro na leitura do arquivo' })
+  }
 });
 
 app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}`));
