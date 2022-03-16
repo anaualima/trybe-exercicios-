ad8b54374d57d4f1270486fd6fa9fa27e2988f99
Alissandra <alissandraraysa@gmail.com>
2022-03-16 06:55:31 -0300

    exercicios dia 22.3

--- back-end/bloco-22/dia-22.3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/calcString.js
+++ back-end/bloco-22/dia-22.3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/calcString.js
@@ -1,9 +1,15 @@
 module.exports = (number) => {
-  if(number > 0){
+
+  if (typeof number !== 'number') {
+    return 'o parâmetro deve ser um número';
+  }
+  if (number > 0) {
     return 'positivo';
   } else if (number < 0) {
     return 'negativo';
   } else {
     return 'neutro';
   }
+
 }
+
--- back-end/bloco-22/dia-22.3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/calcString.test.js
+++ back-end/bloco-22/dia-22.3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/calcString.test.js
@@ -3,23 +3,29 @@ const { expect } = require('chai');
 const calcString = require('../calcString');
 
 describe('Testar se o reultado da funcao é positivo, negativo ou neutro', () => {
-    it('a funcao existe', () => {
-      expect(calcString).to.be.a('function')
-    });
-    it('a funcao retorna uma string', () => {
-      const resultado = calcString(10);
-      expect(resultado).to.be.a('string')
-      });
-    it('tem um resultado positivo', () => {
-      const resultado = calcString(10);
-      expect(resultado).to.be.equals('positivo')
-    });
-    it('tem um resultado negativo', () => {
-      const resultado = calcString(-10);
-      expect(resultado).to.be.equals('negativo')
-    });
-    it('tem um resultado neutro', () => {
-      const resultado = calcString(0);
-      expect(resultado).to.be.equals('neutro')
-    });
+  it('a funcao existe', () => {
+    expect(calcString).to.be.a('function')
   });
+  it('a funcao retorna uma string', () => {
+    const resultado = calcString(10);
+    expect(resultado).to.be.a('string')
+  });
+  it('tem um resultado positivo', () => {
+    const resultado = calcString(10);
+    expect(resultado).to.be.equals('positivo')
+  });
+  it('tem um resultado negativo', () => {
+    const resultado = calcString(-10);
+    expect(resultado).to.be.equals('negativo')
+  });
+  it('tem um resultado neutro', () => {
+    const resultado = calcString(0);
+    expect(resultado).to.be.equals('neutro')
+  });
+});
+describre('A funcao recebe um tratamento caso a entrada não seja um número',() => {
+  it('A entrada deve ser um numero', () => {
+    const resultado = calcString('ola');
+    expect(resultado).to.be.a('string')
+  })
+});
