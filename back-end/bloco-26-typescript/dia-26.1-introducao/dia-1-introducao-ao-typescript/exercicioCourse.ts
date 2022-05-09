98afd62839872991060c2ddc5dbf127421e23c0b
Alissandra <alissandraraysa@gmail.com>
2022-05-09 14:39:25 -0300

    aprendendo typescript

---
+++ back-end/bloco-26-typescript/dia-26.1-introducao/dia-1-introducao-ao-typescript/exercicioCourse.ts
@@ -0,0 +1,34 @@
+enum semana {
+  segunda = 1,
+  terça,
+  quarta,
+  quinta,
+  sexta,
+  sabado,
+  domingo
+}
+
+enum RainbowColors {
+  Red = "Vermelho",
+  Orange = "Laranja",
+  Yellow = "Amarelo",
+  Green = "Verde",
+  Blue = "Azul",
+  Indigo = "Anil",
+  Violet = "Violeta",
+}
+
+enum Actions {
+  Save,
+  Print,
+  Open,
+  View,
+  Close,
+}
+
+enum CardinalPoints {
+  North = "N",
+  East = "E",
+  South = "S",
+  West = "W",
+}
