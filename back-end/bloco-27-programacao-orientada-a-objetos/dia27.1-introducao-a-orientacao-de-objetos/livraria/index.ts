aac6b7dd000fba60ad02e5a1361cc7f36f635ed2
Alissandra <alissandraraysa@gmail.com>
2022-05-23 10:01:17 -0300

    estudando POO

--- back-end/bloco-27-programacao-orientada-a-objetos/dia27.1-introducao-a-orientacao-de-objetos/livraria/index.ts
+++ back-end/bloco-27-programacao-orientada-a-objetos/dia27.1-introducao-a-orientacao-de-objetos/livraria/index.ts
@@ -4,10 +4,14 @@ export default class Book {
   private _salePrice: number;
   private _cost: number;
 
-  constructor(i: string, t: string, s: number, c: number) {
+  constructor(i: string, t: string, c: number, s?: number) {
     this._id = i;
     this._title = t;
-    this._salePrice = s;
+    if(s){
+      this._salePrice = s;
+    }else {
+      this._salePrice = c;
+    }
     this._cost = c;
   }
 
@@ -29,21 +33,47 @@ export default class Book {
     } else {
       this._salePrice = price;
     }
+  }
 
+  public calculateProfit(): number {
+    return this._salePrice - this._cost
+  }
+
+  public setMinimunSalePrice(profitMargin:number): number {
+    const newPrice = this._cost * (1 + profitMargin/100)
+
+    return this._salePrice = newPrice;
   }
 
   get cost() {
     return this._cost;
 
   }
+
+  public toString(): string {
+    return `Livro ${this._title}, preço ${this._salePrice}`
+  }
 }
 
-const b1 = new Book('056781', 'Livretin', 20.00, 10.00);
+const b1 = new Book('056781', 'Livretin', 38.00, 50.00);
 
-const b2 = new Book('056782', 'Folhetin', 60.00, 50.00);
+const b2 = new Book('056782', 'Folhetin', 32.00, 40.00);
 
-console.log('Book 1:', b1, 'Book 2:', b2);
+//exemplo 1
+// console.log('Book 1:', b1, 'Book 2:', b2);
+// console.log(b1.id);
 
-console.log(b1.id);
+// exemplo 2
+// console.log(b2);
+// console.log('Lucro do livro2', b2.calculateProfit());
+// console.log('Setando o valor garantindo 20% de lucro', b2.setMinimunSalePrice(20));
+// console.log(b2);
+
+//exeplo 3
+console.log(`${b2}`);
+
+
+
+
 
 
---
+++ back-end/bloco-27-programacao-orientada-a-objetos/dia27.1-introducao-a-orientacao-de-objetos/projeto/src/app.ts
@@ -0,0 +1,12 @@
+
+  
+import express from "express";
+// import productRouter from "./routes/productRouter";
+
+const app = express();
+
+app.use(express.json()); 
+
+// app.use(productRouter);
+
+export default app;
